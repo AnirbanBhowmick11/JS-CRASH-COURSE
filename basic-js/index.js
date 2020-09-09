6e137beca083b3207cb403f2f3386b90307049d1
AnirbanBhowmick11 <70017851+AnirbanBhowmick11@users.noreply.github.com>
2020-09-09 21:17:20 +0530

    Add files via upload

---
+++ basic-js/index.js
@@ -0,0 +1,205 @@
+// let name = 'Anirban';
+// console.log(name);
+
+// cannot use reserved keyword
+// meaningful name
+// name cannot start with a number
+// cannot have a space or hyphen (-)
+// is case-sensitive
+
+// let firstName = 'Rohit';
+// let lastName;
+// let age=30;
+
+// const discountRate = 0.5;
+//     discountRate = 2;
+//     console.log(discountRate);
+
+
+// let name = 'Anirban';
+
+// 2 Category Of Types:
+// ------------------------
+// Primitive/Value Types
+// Reference Type 
+
+
+// let city = 'Kolkata'; //STRING LITERAL
+// let marks = 50; // NUMBER LITERAL
+// let isApproved = true; // BOOLEAN
+// let age = undefined; // UNDEFINED
+// let selectColor = null; // NULL
+
+
+
+// let person = {
+
+//     name : 'Anirban',
+//     age : 23
+
+// };
+
+// Dot Notation
+// person.name = 'Abhishek';
+
+// console.log(person.name);
+
+// Bracket Notation
+// person['age'] = 25;
+
+// console.log(person['age']);
+// console.log(person);
+
+// let players = ['Anirban','Rohit'];
+//     players[2] = 'Jasmine';
+//     players[3] = 45;
+// console.log(players);
+
+
+// Function Decleration
+// function greet(name, lastName){
+
+//     console.log('Hello ' + name + ' ' + lastName);
+
+// }
+
+function square(number){
+    return number*number;
+}
+
+// Function Calling
+let result = square(5);
+
+console.log(result*2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
